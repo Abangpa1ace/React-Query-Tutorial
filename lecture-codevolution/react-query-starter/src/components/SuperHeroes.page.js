b7034c6aa5bb91cf429bc4482a7a7ec0849e867f
Abangpa1ace <dyek72731@gmail.com>
2022-07-26 22:35:13 +0900

    useQuery 적용

--- lecture-codevolution/react-query-starter/src/components/SuperHeroes.page.js
+++ lecture-codevolution/react-query-starter/src/components/SuperHeroes.page.js
@@ -1,27 +1,36 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const SuperHeroesPage = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  // 1) Error 메세지 상태값 추가
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then(res => {
-      setData(res.data)
-      setIsLoading(false)
-    })
-  }, [])
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      // 2) catch문으로 패치 시 Error 처리 추가
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
 
-  if (isLoading) {
-    return <h2>Loading...</h2>
-  }
+  if (isLoading) return <h2>Loading...</h2>;
+
+  if (error) return <h3>{error}</h3>;
 
   return (
     <>
       <h2>Super Heroes Page</h2>
-      {data.map(hero => {
-        return <div>{hero.name}</div>
+      {data.map((hero) => {
+        return <div>{hero.name}</div>;
       })}
     </>
-  )
-}
+  );
+};
